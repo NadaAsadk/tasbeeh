5cd41a73ec1a54137178cb73177f479fd510ad27
NadaAsadk <nadaasad2003@gmail.com>
2024-02-13 20:55:21 +0200

    update

--- assets/main.js
+++ assets/main.js
@@ -1,43 +1,15 @@
-const sobhan = document.querySelector(".sobhan button");
-const hamd = document.querySelector(".hamd button");
-const la = document.querySelector(".la button");
-const akbar = document.querySelector(".akbar button");
-const reset = document.querySelector(".reset");
-let num = [0,0,0,0];
-let buttons = [];
-buttons.push(sobhan);
-buttons.push(hamd);
-buttons.push(la);
-buttons.push(akbar);
-function addcounter(){
-    let current;
-    for(let i=0;i<4;i++){
-        current = buttons[i].nextSibling;
-        current.textContent=num[i];
-        console.log(current);
-    }
-    
+
+function addcounter(name){
+
+    var element = document.getElementById(name).textContent;
+    element++;
+    document.getElementById(name).textContent=element;
+
 }
 
-addcounter();
-sobhan.onclick = function(e){
-    num[0]++;
-    addcounter();
-}
-hamd.onclick = function(e){
-    num[1]++;
-    addcounter();
-}
-la.onclick = function(e){
-    num[2]++;
-    addcounter();
-}
-akbar.onclick = function(e){
-    num[3]++;
-    addcounter();
-}
-reset.onclick = function(){
-    for(let i=0;i<4;i++)
-    num[i]=0;
-    addcounter();
+function reset(name){
+    var element = document.getElementById(name).textContent;
+    element=0;
+    document.getElementById(name).textContent=element;
+
 }
